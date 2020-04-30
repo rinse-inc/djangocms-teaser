7badb31e1a0807e097c09b1c6e37113a9c97f15a
Brett Jackson <brett.jackson@rinse.com>
2020-04-30 17:42:07 -0500

    import python_2_unicode_compatible from django.utils.six

--- djangocms_teaser/models.py
+++ djangocms_teaser/models.py
@@ -13,7 +13,7 @@ except ImportError:
         for information
         """
         return instance.get_media_path(filename)
-from cms.utils.compat.dj import python_2_unicode_compatible
+from django.utils.six import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
